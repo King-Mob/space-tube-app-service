2876c573a65e898536428213ffa4ee2fd9c0567a
john evans <john.software.evans@gmail.com>
2023-07-04 14:36:24 +0000

    add http event

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import { AppService, AppserviceHttpError } from "matrix-appservice";
 const as = new AppService({
   homeserverToken: process.env.HOME_SERVER_TOKEN
 });
+as.on("http-log",(event)=>{
+  console.log(event);
+  console.log("something happened");
+});
 as.on("type:m.room.message", (event) => {
   // handle the incoming message
   console.log(event);
