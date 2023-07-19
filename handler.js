4debc924dcff77eec56a9e28885c9b5727696e7d
John Evans <38507954+King-Mob@users.noreply.github.com>
2023-07-19 17:42:10 +0100

    add log for passing message on

--- handler.js
+++ handler.js
@@ -431,7 +431,7 @@ export const handleMessage = async (event) => {
                 return;
         }
 
-
+        console.log("passing message to tube intermediary")
 
         const { tubeIntermediary } = tubeOpen.content;
 
