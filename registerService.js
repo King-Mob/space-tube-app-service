216a802d663804528cd788d92d1aeb0f755d9b4f
John Evans <38507954+King-Mob@users.noreply.github.com>
2023-08-08 12:59:26 +0100

    add logging messages for registration script

--- registerService.js
+++ registerService.js
@@ -3,6 +3,7 @@ import { AppServiceRegistration } from "matrix-appservice";
 
 
 // creating registration files
+console.log("Creating the registration yaml to be linked in your homeserver.")
 const reg = new AppServiceRegistration();
 reg.setAppServiceUrl(`http://${process.env.HOST}:8133`);
 reg.setHomeserverToken(AppServiceRegistration.generateToken());
@@ -14,3 +15,4 @@ reg.addRegexPattern("aliases", "#_space-tube.*", true);
 reg.setProtocols(["spacetubeservice"]); // For 3PID lookups
 reg.setId("space-tube-service");
 reg.outputAsYaml("registration.yaml");
+console.log("Done: registration.yaml exists and can be reference in your homeserver.")
