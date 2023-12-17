09bf29155e136e6ec84df6b3b58396db3e256f29
KingMob <john@onethreethree.net>
2023-12-17 15:39:17 +0000

    add discord command

--- discord/index.js
+++ discord/index.js
@@ -1,40 +1,51 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import { verifyKey, InteractionType, InteractionResponseType } from 'discord-interactions';
-import { storeItem, getItem, getDisplayName } from '../storage.js';
-import { createRoom, invite, join, registerUser, sendMessageAsUser } from '../handler.js';
+import express from "express";
+import fetch from "node-fetch";
+import {
+  verifyKey,
+  InteractionType,
+  InteractionResponseType,
+} from "discord-interactions";
+import { storeItem, getItem, getDisplayName } from "../storage.js";
+import {
+  createRoom,
+  invite,
+  join,
+  registerUser,
+  sendMessageAsUser,
+} from "../handler.js";
 
 export async function DiscordRequest(endpoint, options) {
-    // append endpoint to root API URL
-    const url = 'https://discord.com/api/v10/' + endpoint;
-    // Stringify payloads
-    if (options.body) options.body = JSON.stringify(options.body);
-    // Use node-fetch to make requests
-    const res = await fetch(url, {
-        headers: {
-            Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
-            'Content-Type': 'application/json; charset=UTF-8',
-            'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
-        },
-        ...options
-    });
-    // throw API errors
-    if (!res.ok) {
-        const data = await res.json();
-        console.log(res.status);
-        throw new Error(JSON.stringify(data));
-    }
-    // return original response
-    return res;
+  // append endpoint to root API URL
+  const url = "https://discord.com/api/v10/" + endpoint;
+  // Stringify payloads
+  if (options.body) options.body = JSON.stringify(options.body);
+  // Use node-fetch to make requests
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      "Content-Type": "application/json; charset=UTF-8",
+      "User-Agent":
+        "DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)",
+    },
+    ...options,
+  });
+  // throw API errors
+  if (!res.ok) {
+    const data = await res.json();
+    console.log(res.status);
+    throw new Error(JSON.stringify(data));
+  }
+  // return original response
+  return res;
 }
 
 export async function sendMessageDiscord(event, bridgeRoom) {
-    console.log(event);
-    console.log(bridgeRoom);
-    console.log("sending message to discord")
-    // send message as the space tube bot user
+  console.log(event);
+  console.log(bridgeRoom);
+  console.log("sending message to discord");
+  // send message as the space tube bot user
 
-    /*
+  /*
     DiscordRequest(`/channels/${bridgeRoom.channelId}/messages`, {
         method: "POST",
         body: {
@@ -42,182 +53,234 @@ export async function sendMessageDiscord(event, bridgeRoom) {
         }
     });*/
 
-    let webhook;
-    const webhookEvent = await getItem("channelId", bridgeRoom.channelId, "spacetube.create.webhook");
-    if (webhookEvent) {
-        webhook = webhookEvent.content;
-    }
-    else {
-        const webhookResponse = await DiscordRequest(`/channels/${bridgeRoom.channelId}/webhooks`, {
-            method: 'POST',
-            body: {
-                name: "spacetube-webhook"
-            }
-        })
-        webhook = await webhookResponse.json();
-        webhook.channelId = bridgeRoom.channelId;
-
-        await storeItem({
-            ...webhook,
-            type: "spacetube.create.webhook"
-        });
-    }
-
-    console.log(webhook)
-
-    const displayName = await getDisplayName(event.room_id, event.sender);
-
-    DiscordRequest(`/webhooks/${webhook.id}/${webhook.token}`, {
+  let webhook;
+  const webhookEvent = await getItem(
+    "channelId",
+    bridgeRoom.channelId,
+    "spacetube.create.webhook"
+  );
+  if (webhookEvent) {
+    webhook = webhookEvent.content;
+  } else {
+    const webhookResponse = await DiscordRequest(
+      `/channels/${bridgeRoom.channelId}/webhooks`,
+      {
         method: "POST",
         body: {
-            content: event.content.body,
-            username: displayName || event.sender
-        }
-    })
+          name: "spacetube-webhook",
+        },
+      }
+    );
+    webhook = await webhookResponse.json();
+    webhook.channelId = bridgeRoom.channelId;
 
-    // sending message using webhook
+    await storeItem({
+      ...webhook,
+      type: "spacetube.create.webhook",
+    });
+  }
 
+  console.log(webhook);
+
+  const displayName = await getDisplayName(event.room_id, event.sender);
+
+  DiscordRequest(`/webhooks/${webhook.id}/${webhook.token}`, {
+    method: "POST",
+    body: {
+      content: event.content.body,
+      username: displayName || event.sender,
+    },
+  });
+
+  // sending message using webhook
 }
 
 function VerifyDiscordRequest(clientKey) {
-    return function (req, res, buf, encoding) {
-        console.log("verifying")
-        const signature = req.get('X-Signature-Ed25519');
-        const timestamp = req.get('X-Signature-Timestamp');
+  return function (req, res, buf, encoding) {
+    console.log("verifying");
+    const signature = req.get("X-Signature-Ed25519");
+    const timestamp = req.get("X-Signature-Timestamp");
 
-        const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
-        if (!isValidRequest) {
-            res.status(401).send('Bad request signature');
-            throw new Error('Bad request signature');
-        }
-        console.log("valid request", isValidRequest)
-    };
+    const isValidRequest = verifyKey(buf, signature, timestamp, clientKey);
+    if (!isValidRequest) {
+      res.status(401).send("Bad request signature");
+      throw new Error("Bad request signature");
+    }
+    console.log("valid request", isValidRequest);
+  };
 }
 
 export const startDiscord = (app) => {
-    app.use(express.json({ verify: VerifyDiscordRequest(process.env.DISCORD_PUBLIC_KEY) }));
+  app.use(
+    express.json({
+      verify: VerifyDiscordRequest(process.env.DISCORD_PUBLIC_KEY),
+    })
+  );
 
-    app.post('/interactions', async function (req, res) {
-        // Interaction type and data
-        const { body, body: { type, data } } = req;
+  app.post("/interactions", async function (req, res) {
+    // Interaction type and data
+    const {
+      body,
+      body: { type, data },
+    } = req;
 
-        /**
-         * Handle verification requests
-         */
-        if (type === InteractionType.PING) {
-            console.log("ping")
-            return res.send({ type: InteractionResponseType.PONG });
-        }
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+      console.log("ping");
+      return res.send({ type: InteractionResponseType.PONG });
+    }
 
-        if (data.name === "echo") {
-            const message = data.options[0].value;
+    if (data.name === "echo") {
+      const message = data.options[0].value;
 
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: `you said: ${message}`,
-                },
-            });
-        }
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `you said: ${message}`,
+        },
+      });
+    }
 
-        if (data.name === "create") {
-            console.log(body);
-            const groupName = data.options[0].value;
+    if (data.name === "create") {
+      console.log(body);
+      const groupName = data.options[0].value;
 
-            let room;
+      let room;
 
-            const bridgeRoomEvent = await getItem("channelId", body.channel_id);
-            console.log(bridgeRoomEvent);
-            if (bridgeRoomEvent) {
-                room = bridgeRoomEvent.content;
-                room.room_id = room.roomId;
-            }
-            else {
-                const roomResponse = await createRoom(groupName);
-                room = await roomResponse.json();
+      const bridgeRoomEvent = await getItem("channelId", body.channel_id);
+      console.log(bridgeRoomEvent);
+      if (bridgeRoomEvent) {
+        room = bridgeRoomEvent.content;
+        room.room_id = room.roomId;
+      } else {
+        const roomResponse = await createRoom(groupName);
+        room = await roomResponse.json();
 
-                await storeItem({
-                    type: "spacetube.bridge.create",
-                    service: "discord",
-                    bridgeRoomId: room.room_id,
-                    roomId: room.room_id,
-                    channelId: body.channel_id,
-                    guildId: body.guild_id
-                });
-            }
+        await storeItem({
+          type: "spacetube.bridge.create",
+          service: "discord",
+          bridgeRoomId: room.room_id,
+          roomId: room.room_id,
+          channelId: body.channel_id,
+          guildId: body.guild_id,
+        });
+      }
 
-            console.log(room)
+      console.log(room);
 
-            let user;
-            const bridgeUserEvent = await getItem("bridgeUserRoomId", room.room_id);
-            if (bridgeUserEvent) {
-                user = bridgeUserEvent.content;
-                user.access_token = user.user.access_token;
-            }
-            else {
-                const userResponse = await registerUser(`disco-${groupName}`);
-                user = await userResponse.json();
+      let user;
+      const bridgeUserEvent = await getItem("bridgeUserRoomId", room.room_id);
+      if (bridgeUserEvent) {
+        user = bridgeUserEvent.content;
+        user.access_token = user.user.access_token;
+      } else {
+        const userResponse = await registerUser(`disco-${groupName}`);
+        user = await userResponse.json();
 
-                await storeItem({
-                    type: "spacetube.bridge.user.create",
-                    userId: user.user_id,
-                    user: user,
-                    bridgeUserRoomId: room.room_id,
-                    userRoomId: room.room_id,
-                    guildId: body.guild_id,
-                    channelId: body.channel_id
-                });
-            }
+        await storeItem({
+          type: "spacetube.bridge.user.create",
+          userId: user.user_id,
+          user: user,
+          bridgeUserRoomId: room.room_id,
+          userRoomId: room.room_id,
+          guildId: body.guild_id,
+          channelId: body.channel_id,
+        });
+      }
 
-            await invite(user, room.room_id);
-            await join(user, room.room_id);
+      await invite(user, room.room_id);
+      await join(user, room.room_id);
 
-            sendMessageAsUser(user, room.room_id, "!space-tube create");
+      sendMessageAsUser(user, room.room_id, "!space-tube create");
 
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: `creating a new tube`,
-                },
-            });
-        }
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `creating a new tube`,
+        },
+      });
+    }
 
-        if (data.name === "connect") {
-            const connectionCode = data.options[0].value;
+    if (data.name === "connect") {
+      const connectionCode = data.options[0].value;
 
-            const bridgeRoomEvent = await getItem("channelId", body.channel_id, "spacetube.bridge.create");
-            const bridgeRoom = bridgeRoomEvent.content;
+      const bridgeRoomEvent = await getItem(
+        "channelId",
+        body.channel_id,
+        "spacetube.bridge.create"
+      );
+      const bridgeRoom = bridgeRoomEvent.content;
 
-            const bridgeUserEvent = await getItem("bridgeUserRoomId", bridgeRoom.roomId);
-            const bridgeUser = bridgeUserEvent.content;
+      const bridgeUserEvent = await getItem(
+        "bridgeUserRoomId",
+        bridgeRoom.roomId
+      );
+      const bridgeUser = bridgeUserEvent.content;
 
-            sendMessageAsUser(bridgeUser.user, bridgeRoom.roomId, `!space-tube connect ${connectionCode}`);
+      sendMessageAsUser(
+        bridgeUser.user,
+        bridgeRoom.roomId,
+        `!space-tube connect ${connectionCode}`
+      );
 
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: `connecting you to the tube`,
-                },
-            });
-        }
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `connecting you to the tube`,
+        },
+      });
+    }
 
-        if (data.name === "send") {
-            const message = data.options[0].value;
+    if (data.name === "send") {
+      const message = data.options[0].value;
 
-            const bridgeRoomEvent = await getItem("channelId", body.channel_id, "spacetube.bridge.create");
-            const bridgeRoom = bridgeRoomEvent.content;
+      const bridgeRoomEvent = await getItem(
+        "channelId",
+        body.channel_id,
+        "spacetube.bridge.create"
+      );
+      const bridgeRoom = bridgeRoomEvent.content;
 
-            const bridgeUserEvent = await getItem("bridgeUserRoomId", bridgeRoom.roomId);
-            const bridgeUser = bridgeUserEvent.content;
+      const bridgeUserEvent = await getItem(
+        "bridgeUserRoomId",
+        bridgeRoom.roomId
+      );
+      const bridgeUser = bridgeUserEvent.content;
 
-            sendMessageAsUser(bridgeUser.user, bridgeRoom.roomId, message);
+      sendMessageAsUser(bridgeUser.user, bridgeRoom.roomId, message);
 
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: `sending to the tube`,
-                },
-            });
-        }
-    });
-}
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `sending to the tube`,
+        },
+      });
+    }
+
+    if (data.name === "link") {
+      const bridgeRoomEvent = await getItem(
+        "channelId",
+        body.channel_id,
+        "spacetube.bridge.create"
+      );
+      const bridgeRoom = bridgeRoomEvent.content;
+
+      const bridgeUserEvent = await getItem(
+        "bridgeUserRoomId",
+        bridgeRoom.roomId
+      );
+      const bridgeUser = bridgeUserEvent.content;
+
+      sendMessageAsUser(bridgeUser.user, bridgeRoom.roomId, `!spacetube link`);
+
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: `generating tube link`,
+        },
+      });
+    }
+  });
+};
--- discord/registerDiscord.js
+++ discord/registerDiscord.js
@@ -1,73 +1,85 @@
-import 'dotenv/config';
-import { DiscordRequest } from './index.js';
+import "dotenv/config";
+import { DiscordRequest } from "./index.js";
 
 const ECHO_COMMAND = {
-    name: 'echo',
-    description: 'echo what has been sent',
-    type: 1,
-    options: [
-        {
-            name: "message",
-            description: "what you want to repeat",
-            type: 3,
-            required: true
-        }
-    ]
+  name: "echo",
+  description: "echo what has been sent",
+  type: 1,
+  options: [
+    {
+      name: "message",
+      description: "what you want to repeat",
+      type: 3,
+      required: true,
+    },
+  ],
 };
 
 const CREATE_COMMAND = {
-    name: "create",
-    description: "create a space-tube opening",
-    type: 1,
-    options: [
-        {
-            name: "group_name",
-            description: "what you want your group to appear as",
-            type: 3,
-            required: true
-        }
-    ]
-}
+  name: "create",
+  description: "create a space-tube opening",
+  type: 1,
+  options: [
+    {
+      name: "group_name",
+      description: "what you want your group to appear as",
+      type: 3,
+      required: true,
+    },
+  ],
+};
 
 const CONNECT_COMMAND = {
-    name: "connect",
-    description: "connect to another space-tube opening",
-    type: 1,
-    options: [
-        {
-            name: "connection_code",
-            description: "the other group's connection code",
-            type: 3,
-            required: true
-        }
-    ]
-}
+  name: "connect",
+  description: "connect to another space-tube opening",
+  type: 1,
+  options: [
+    {
+      name: "connection_code",
+      description: "the other group's connection code",
+      type: 3,
+      required: true,
+    },
+  ],
+};
 const SEND_COMMAND = {
-    name: "send",
-    description: "send a message through a space tube",
-    type: 1,
-    options: [
-        {
-            name: "message",
-            description: "the text of what you want to send",
-            type: 3,
-            required: true
-        }
-    ]
-}
+  name: "send",
+  description: "send a message through a space tube",
+  type: 1,
+  options: [
+    {
+      name: "message",
+      description: "the text of what you want to send",
+      type: 3,
+      required: true,
+    },
+  ],
+};
+const LINK_COMMAND = {
+  name: "link",
+  description: "get a link to view your spacetube",
+  type: 1,
+  options: [],
+};
 
-const ALL_COMMANDS = [ECHO_COMMAND, CREATE_COMMAND, CONNECT_COMMAND, SEND_COMMAND];
+const ALL_COMMANDS = [
+  ECHO_COMMAND,
+  CREATE_COMMAND,
+  CONNECT_COMMAND,
+  SEND_COMMAND,
+  LINK_COMMAND,
+];
 
 async function InstallGlobalCommands(appId, commands) {
-    // API endpoint to overwrite global commands
-    const endpoint = `applications/${appId}/commands`;
+  // API endpoint to overwrite global commands
+  const endpoint = `applications/${appId}/commands`;
 
-    try {
-        // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
-        await DiscordRequest(endpoint, { method: 'PUT', body: commands });
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
+    await DiscordRequest(endpoint, { method: "PUT", body: commands });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 InstallGlobalCommands(process.env.DISCORD_APP_ID, ALL_COMMANDS);
