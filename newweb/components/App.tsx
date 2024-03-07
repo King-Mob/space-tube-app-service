8360c5756ecb95505d5a87c1c9f7eb8fe043304f
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-03-07 15:56:00 +0000

    render tube events

--- newweb/components/App.tsx
+++ newweb/components/App.tsx
@@ -1,5 +1,5 @@
 import "../styles/app.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const { HOME_SERVER } = process.env;
 const url = `https://spacetube.${HOME_SERVER}`;
@@ -9,6 +9,18 @@ const urlParams = new URLSearchParams(queryString);
 
 const linkToken = urlParams.get("linkToken");
 const displayName = urlParams.get("name");
+
+const connectLinkedEvents = (events) => {
+  const linkedEvents = {};
+  events.forEach((event) => {
+    if (event.type === "spacetube.forward" && event.content.originalEventId)
+      linkedEvents[event.content.originalEventId] = event;
+  });
+  return events.map((event) => {
+    event.connectedEvent = linkedEvents[event.event_id];
+    return event;
+  });
+};
 
 const getRooms = async (user) => {
   const tubeInfoResponse = await fetch(
@@ -26,11 +38,12 @@ const getRooms = async (user) => {
     }
   );
   const eventsList = await matrixRoomResponse.json();
+  const connectedEvents = connectLinkedEvents(eventsList.chunk);
 
   return {
     matrixRoom: {
       roomId: tubeInfo.matrixRoomId,
-      events: eventsList.chunk,
+      events: connectedEvents,
     },
     tubeRoom: {
       name: tubeInfo.tubeRoomName,
@@ -69,17 +82,14 @@ const registerUser = async () => {
     return;
   }
 };
-
 const App = () => {
   const [user, setUser] = useState();
   const [matrixRoom, setMatrixRoom] = useState();
   const [tubeRoom, setTubeRoom] = useState();
   const [message, setMessage] = useState("");
   const [sendDisabled, setSendDisabled] = useState(true);
-
-  console.log(matrixRoom);
-  console.log(tubeRoom);
-  console.log(message);
+  const tubeMessageEnd = useRef();
+  const matrixMessageEnd = useRef();
 
   useEffect(() => {
     const storedUser = localStorage.getItem(`spacetube-user-${linkToken}`);
@@ -90,15 +100,27 @@ const App = () => {
     }
   }, []);
 
-  useEffect(() => {
-    const setUpRooms = async () => {
-      if (user) {
-        const { matrixRoom, tubeRoom } = await getRooms(user);
-        setMatrixRoom(matrixRoom);
-        setTubeRoom(tubeRoom);
-      }
-    };
+  const scrollToBottom = () => {
+    tubeMessageEnd.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+    matrixMessageEnd.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  };
 
+  const setUpRooms = async (scroll = true) => {
+    if (user) {
+      const { tubeRoom, matrixRoom } = await getRooms(user);
+      setTubeRoom(tubeRoom);
+      setMatrixRoom(matrixRoom);
+      if (scroll) setTimeout(scrollToBottom, 500);
+    }
+  };
+
+  useEffect(() => {
     setUpRooms();
   }, [user]);
 
@@ -107,6 +129,13 @@ const App = () => {
       setSendDisabled(false);
     }
   }, [matrixRoom]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setUpRooms(false);
+    }, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   const sendEvent = async () => {
     fetch(
@@ -123,29 +152,117 @@ const App = () => {
         },
       }
     );
+
+    setMessage("");
+    setUpRooms();
   };
 
-  // rendering events in the rooms
-  // scrolling to the bottom
+  const forward = async (event) => {
+    if (confirm("Send this message to the tube?")) {
+      const txnId = self.crypto.randomUUID();
 
-  // checking for new events every second with an interval.
-  // gotta be a better way than doing that
+      await fetch(
+        `https://matrix.${HOME_SERVER}/_matrix/client/v3/rooms/${matrixRoom.roomId}/send/spacetube.forward/${txnId}?user_id=@${user.userId}`,
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            type: "spacetube.forward",
+            body: event.content.body,
+            originalEventId: event.event_id,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.accessToken}`,
+          },
+        }
+      );
+
+      setTimeout(setUpRooms, 200);
+    }
+  };
+
+  const tubeRoomNames = {};
+
+  const tubeRoomEvents =
+    tubeRoom &&
+    tubeRoom.events.map((event) => {
+      if (event.type === "m.room.message") {
+        return (
+          <div className="message-container">
+            <p className="name">
+              {tubeRoomNames[event.sender] || event.sender}
+            </p>
+            <p>{event.content.body}</p>
+          </div>
+        );
+      }
+      if (event.type === "m.room.member" && event.content.displayname) {
+        const displayName = event.content.displayname;
+        tubeRoomNames[event.sender] = displayName;
+      }
+      return null;
+    });
+
+  const matrixRoomNames = {};
+  let matrixRoomTitle;
+
+  const matrixRoomEvents =
+    matrixRoom &&
+    matrixRoom.events.map((event) => {
+      if (event.type === "m.room.name") {
+        matrixRoomTitle = "🏠" + event.content.name;
+      }
+      if (event.type === "m.room.message") {
+        return (
+          <div className="message-container" onClick={() => forward(event)}>
+            <p className="name">
+              {matrixRoomNames[event.sender] || event.sender}
+            </p>
+            <p>{event.content.body}</p>
+            {event.connectedEvent && (
+              <p className="forwarded">
+                forwarded to tube by{" "}
+                {matrixRoomNames[event.connectedEvent.sender] ||
+                  event.connectedEvent.sender}
+              </p>
+            )}
+          </div>
+        );
+      }
+      if (
+        event.type === "m.room.member" &&
+        event.content.displayname &&
+        event.content.membership === "join"
+      ) {
+        matrixRoomNames[event.sender] = event.content.displayname;
+      }
+      return null;
+    });
+
+  const roomParticipants = Object.values(tubeRoomNames).filter(
+    (name: string) => !name.includes("_")
+  );
+  const participantList = roomParticipants.join(" + ");
+  const tubeRoomTitle = `🛸🛸${participantList}🛸🛸`;
 
   return (
     <>
       <div id="tube-room-container" className="room-container">
-        <h1 id="tube-room-title">Tube Room</h1>
-        <div
-          id="tube-room-events-container"
-          className="room-events-container"
-        ></div>
+        <h1 id="tube-room-title">{tubeRoomTitle || "Tube Room"}</h1>
+        <div id="tube-room-events-container" className="room-events-container">
+          {tubeRoomEvents}
+          <div ref={tubeMessageEnd} />
+        </div>
       </div>
       <div id="matrix-room-container" className="room-container">
-        <h1 id="matrix-room-title">Matrix Room</h1>
+        <h1 id="matrix-room-title">{matrixRoomTitle || "Matrix Room"}</h1>
         <div
           id="matrix-room-events-container"
           className="room-events-container"
-        ></div>
+        >
+          {matrixRoomEvents}
+          <div ref={matrixMessageEnd} />
+        </div>
         <div id="new-message-container">
           <input
             type="text"
