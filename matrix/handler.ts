b95e6a64bab17532aa0720627dec689784877356
John Evans <38507954+King-Mob@users.noreply.github.com>
2024-05-11 12:22:57 +0100

    add logs

--- matrix/handler.ts
+++ matrix/handler.ts
@@ -391,7 +391,9 @@ export const forwardToTubeIntermediary = async (tubeIntermediary, event) => {
   const message = event.content.body;
 
   const inviteUser = await getItem("roomId", event.room_id, "spacetube.group.invite");
+  console.log(inviteUser)
   const groupUser = await getItem("originalUserId", inviteUser.content.originalUserId, "spacetube.group.user");
+  console.log(groupUser)
 
   sendMessageAsUser(groupUser.content.user, tubeIntermediary, message);
 
