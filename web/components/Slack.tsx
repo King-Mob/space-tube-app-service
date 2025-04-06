f138cbcbbb5250d6c4ae9d6d1e5687b48e9f0505
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-04-06 17:53:46 +0100

    add stars to page

--- web/components/Slack.tsx
+++ web/components/Slack.tsx
@@ -1,6 +1,9 @@
+import Stars from "./Stars";
+
 const Slack = () => {
   return (
     <div>
+      <Stars />
       <h1>Thanks for Installing Spacetube on Slack</h1>
       <p>you won't regret this</p>
     </div>
