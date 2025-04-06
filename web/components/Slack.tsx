d4a41a7119922a8c17f66a29d5913594f9ecd84b
John Evans <38507954+King-Mob@users.noreply.github.com>
2025-04-06 17:51:42 +0100

    add slack route

---
+++ web/components/Slack.tsx
@@ -0,0 +1,10 @@
+const Slack = () => {
+  return (
+    <div>
+      <h1>Thanks for Installing Spacetube on Slack</h1>
+      <p>you won't regret this</p>
+    </div>
+  );
+};
+
+export default Slack;
--- web/index.tsx
+++ web/index.tsx
@@ -1,10 +1,19 @@
 import "dotenv/config";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./components/App";
+import Slack from "./components/Slack";
 
 console.log("welcome to 🛸s p a c e t u b e🛸");
 
 const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/slack" element={<Slack />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 console.log(process.env.HOME_SERVER);
