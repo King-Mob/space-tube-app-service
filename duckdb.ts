5b9a6a982eba0bc83459aeb7fd518ecdc2f38c2c
KingMob <john@onethreethree.net>
2025-04-08 10:37:41 +0100

    remove seed data

--- duckdb.ts
+++ duckdb.ts
@@ -16,13 +16,6 @@ export async function startDuckDB() {
             "CREATE TABLE ChannelTubeRoomLinks (channel_id VARCHAR, channel_type VARCHAR, tube_room_id VARCHAR);";
         await connection.run(createChannelTubeRoomLinks);
 
-        const insertFirstLink = `INSERT INTO ChannelTubeRoomLinks VALUES ('${SLACK_CHANNEL}', 'slack', '!MPYlXGrdptSscBRvAb:spacetu.be');`;
-        await connection.run(insertFirstLink);
-
-        const insertSecondLink =
-            "INSERT INTO ChannelTubeRoomLinks VALUES ('!tfHSOJOhSOJwHiFolz:spacetu.be', 'matrix', '!MPYlXGrdptSscBRvAb:spacetu.be'); ";
-        await connection.run(insertSecondLink);
-
         const createTubeUserRoomMemberships =
             "CREATE TABLE TubeUserRoomMemberships (tube_user_id VARCHAR, room_id VARCHAR);";
         await connection.run(createTubeUserRoomMemberships);
@@ -34,22 +27,13 @@ export async function startDuckDB() {
         const createSlackChannelTeamLinks = "CREATE TABLE SlackChannelTeamLinks (channel_id VARCHAR, team_id VARCHAR);";
         await connection.run(createSlackChannelTeamLinks);
 
-        const insertSCTL = `INSERT INTO SlackChannelTeamLinks VALUES ('${SLACK_CHANNEL}','${SLACK_TEAM}');`;
-        await connection.run(insertSCTL);
-
         const createSlackTeamBotTokenLinks =
             "CREATE TABLE SlackTeamBotTokenLinks (team_id VARCHAR, bot_token VARCHAR, bot_user_id VARCHAR);";
         await connection.run(createSlackTeamBotTokenLinks);
 
-        const insertSTBTL = `INSERT INTO SlackTeamBotTokenLinks VALUES ('${SLACK_TEAM}','${SLACK_TOKEN}','${SLACK_BOT_USER_ID}')`;
-        await connection.run(insertSTBTL);
-
         const createInviteTubeRoomLinks =
             "CREATE TABLE InviteTubeRoomLinks (invite_code VARCHAR, tube_room_id VARCHAR);";
         await connection.run(createInviteTubeRoomLinks);
-
-        const insertInviteTubeRoomLink = `INSERT INTO InviteTubeRoomLinks VALUES ('${TEST_INVITE_CODE}','!MPYlXGrdptSscBRvAb:spacetu.be');`;
-        await connection.run(insertInviteTubeRoomLink);
 
         console.log("duckdb initiated");
     }
