e48935907ab1c2ab437e7d67038f17c515acab94
KingMob <john@onethreethree.net>
2025-04-08 10:44:23 +0100

    add log

--- web/components/Slack.tsx
+++ web/components/Slack.tsx
@@ -8,39 +8,39 @@ const urlParams = new URLSearchParams(queryString);
 const slack_code = urlParams.get("code");
 
 const Slack = () => {
-  const [success, setSuccess] = useState(false);
+    const [success, setSuccess] = useState(false);
 
-  const processCode = async () => {
-    const processSlackResponse = await processSlackRequest(slack_code);
-    const processSlackResult = await processSlackResponse.json();
+    const processCode = async () => {
+        const processSlackResponse = await processSlackRequest(slack_code);
+        const processSlackResult = await processSlackResponse.json();
+        console.log(processSlackResult);
 
-    if (processSlackResult.success) setSuccess(true);
-  };
+        if (processSlackResult.success) setSuccess(true);
+    };
 
-  useEffect(() => {
-    processCode();
-  }, []);
+    useEffect(() => {
+        processCode();
+    }, []);
 
-  return (
-    <div>
-      <Stars />
-      <h1>Thanks for Installing Spacetube on Slack</h1>
-      <p>you won't regret this</p>
-      {success ? (
-        <>
-          <p>slack OAuth Code processed successfully!</p>
-          <p>Now create the slack channel you want to use spacetube in.</p>
-          <p>Invite @spacetube to the channel</p>
-          <p>
-            And use <span>@spacetube !connect your-invite-code</span> to join a
-            tube!
-          </p>
-        </>
-      ) : (
-        <p>Processing slack OAuth Code</p>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <Stars />
+            <h1>Thanks for Installing Spacetube on Slack</h1>
+            <p>you won't regret this</p>
+            {success ? (
+                <>
+                    <p>slack OAuth Code processed successfully!</p>
+                    <p>Now create the slack channel you want to use spacetube in.</p>
+                    <p>Invite @spacetube to the channel</p>
+                    <p>
+                        And use <span>@spacetube !connect your-invite-code</span> to join a tube!
+                    </p>
+                </>
+            ) : (
+                <p>Processing slack OAuth Code</p>
+            )}
+        </div>
+    );
 };
 
 export default Slack;
