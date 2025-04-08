dbaa515dc2d323b1e31b537c551dad6940422eaa
KingMob <john@onethreethree.net>
2025-04-08 15:57:18 +0100

    add subheadings

--- web/components/Slack.tsx
+++ web/components/Slack.tsx
@@ -39,6 +39,7 @@ const Slack = () => {
                 </p>
                 <p>Read on for the Slack Specifics.</p>
                 <span className="divider"></span>
+                <h3 className="center">Add the Spacetube plugin</h3>
                 {slack_code ? (
                     success ? (
                         <>
@@ -58,8 +59,22 @@ const Slack = () => {
                             <a href="https://slack.com/oauth/v2/authorize?client_id=4433579405958.8709202233781&scope=app_mentions:read,chat:write,chat:write.customize,reactions:read,team:read,users.profile:read">
                                 Slack authorisation flow here
                             </a>
-                            . Afterwards, Slack will send you back to this page. See you soon!
+                            . Or click this big button:
                         </p>
+                        <a
+                            id="slack-button"
+                            href="https://slack.com/oauth/v2/authorize?client_id=4433579405958.8709202233781&scope=app_mentions:read,chat:write,chat:write.customize,reactions:read,team:read,users.profile:read&user_scope="
+                        >
+                            <img
+                                alt="Add to Slack"
+                                height="40"
+                                width="139"
+                                src="https://platform.slack-edge.com/img/add_to_slack.png"
+                                srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                            />
+                        </a>
+                        <p>Afterwards, Slack will send you back to this page. See you soon!</p>
+
                         <p className="center">Once that's done...</p>
                     </>
                 )}
@@ -72,18 +87,21 @@ const Slack = () => {
                     2. Invite @spacetube to the channel. You can use <span>/invite @spacetube</span>.
                 </p>
                 <p className="center">Once that's done, EITHER:</p>
+                <h3 className="center">Create a new tube</h3>
                 <p>
                     1. Send <span>@spacetube !create your-optional-custom-invite-code-here</span> in the Slack channel
                     to make a tube! You can leave out the custom invite code and spacetube will make one for you.
                 </p>
                 <p>2. Share the invite code with someone in the other group. Now they need to do the next part.</p>
                 <p className="center">OR</p>
+                <h3 className="center">Connect to an existing tube</h3>
                 <p>1. Copy the invite code you received from someone else.</p>
                 <p>
                     2. Use the code and send <span>@spacetube !connect received-invite-code-here</span> in the Slack
                     channel to connect to their tube.
                 </p>
                 <p className="center">FINALLY</p>
+                <h3 className="center">Send messages</h3>
                 <p>
                     Send messages by sending <span>@spacetube what's up earthlings?</span> in the Slack channel and
                     everything after @spacetube will be forwarded to those in the tube.
