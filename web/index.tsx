e158d0e942d9deec102a82695fc58bb70ad6d9b4
KingMob <john@onethreethree.net>
2025-04-10 17:12:19 +0100

    create matrix specific instructions

--- web/index.tsx
+++ web/index.tsx
@@ -3,17 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./components/App";
 import Slack from "./components/Slack";
+import Matrix from "./components/Matrix";
 
 console.log("welcome to 🛸s p a c e t u b e🛸");
 
 const root = createRoot(document.getElementById("app"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/slack" element={<Slack />} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/slack" element={<Slack />} />
+            <Route path="/matrix" element={<Matrix />} />
+        </Routes>
+    </BrowserRouter>
 );
 
 console.log(process.env.HOME_SERVER);
